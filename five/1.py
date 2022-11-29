8e85f6e2914ad084436578ac526fdda572f43b6a
dapoadedire <adedireadedapo19@gmail.com>
2022-11-29 17:51:24 +0100

    remove wrong solution

--- five/1.py
+++ five/1.py
@@ -1,1 +1,7 @@
-# research online
+# Discuss the importance of functions in Python
+
+# 1. Functions are used to perform a specific task
+# 2. Functions are used to avoid code repetition
+# 3. Functions are used to make code more readable
+# 4. Functions are used to make code more reusable
+# 5. Functions are used to make code more maintainable.
--- five/8.py
+++
@@ -1,5 +0,0 @@
-word = "My name is John"
-
-encrypted_word = word[::2] + word[::3] + "ay"
-
-print(encrypted_word)
